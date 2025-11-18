66608f05d35a5bfb0f34ec69b340f93646d1ff10
Bufa <julian@bluematador.com>
2025-11-18 16:04:45 -0300

    Ability to schedule Mute Rules

--- src/types.ts
+++ src/types.ts
@@ -277,12 +277,20 @@ export interface MonitorsMuteRuleMap {
   [serviceName: string]: string[];
 }
 
+export interface MuteRuleSchedule {
+  start: string;
+  end: string;
+  timezone: string;
+  repeatIntervalDays?: number;
+}
+
 export interface CreateMuteRuleData {
   hide: boolean;
   resource?: ResourceRef;
   projects?: string[];
   regions?: string[];
   monitors?: MonitorsMuteRuleMap;
+  schedule?: MuteRuleSchedule;
 }
 
 export interface MuteRule {
@@ -292,6 +300,7 @@ export interface MuteRule {
   projects?: string[];
   regions?: string[];
   monitors?: MonitorsMuteRuleMap;
+  schedule?: MuteRuleSchedule;
   active: boolean;
 }
 
